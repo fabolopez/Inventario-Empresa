600072e5ddaac7f1cbd049df2deb73930394651f
= <=>
2024-03-13 17:50:14 -0600

    Primera subida

---
+++ inventarioEmpresa/Form1.Designer.vb
@@ -0,0 +1,240 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dataGridViewProductos = New System.Windows.Forms.DataGridView()
+        Me.labelTitulo = New System.Windows.Forms.Label()
+        Me.btnAgregar = New System.Windows.Forms.Button()
+        Me.btnActualiar = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.txtCodigo = New System.Windows.Forms.TextBox()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtPrecio = New System.Windows.Forms.TextBox()
+        Me.txtCategoria = New System.Windows.Forms.TextBox()
+        Me.txtVendidas = New System.Windows.Forms.TextBox()
+        Me.txtExistentes = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        CType(Me.dataGridViewProductos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dataGridViewProductos
+        '
+        Me.dataGridViewProductos.BackgroundColor = System.Drawing.SystemColors.ControlDark
+        Me.dataGridViewProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dataGridViewProductos.Location = New System.Drawing.Point(44, 36)
+        Me.dataGridViewProductos.Name = "dataGridViewProductos"
+        Me.dataGridViewProductos.Size = New System.Drawing.Size(565, 168)
+        Me.dataGridViewProductos.TabIndex = 0
+        '
+        'labelTitulo
+        '
+        Me.labelTitulo.AutoSize = True
+        Me.labelTitulo.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!)
+        Me.labelTitulo.Location = New System.Drawing.Point(220, 9)
+        Me.labelTitulo.Name = "labelTitulo"
+        Me.labelTitulo.Size = New System.Drawing.Size(197, 24)
+        Me.labelTitulo.TabIndex = 1
+        Me.labelTitulo.Text = "Inventario mi Empresa"
+        '
+        'btnAgregar
+        '
+        Me.btnAgregar.Location = New System.Drawing.Point(547, 246)
+        Me.btnAgregar.Name = "btnAgregar"
+        Me.btnAgregar.Size = New System.Drawing.Size(94, 36)
+        Me.btnAgregar.TabIndex = 2
+        Me.btnAgregar.Text = "Agregar"
+        Me.btnAgregar.UseVisualStyleBackColor = True
+        '
+        'btnActualiar
+        '
+        Me.btnActualiar.Location = New System.Drawing.Point(547, 304)
+        Me.btnActualiar.Name = "btnActualiar"
+        Me.btnActualiar.Size = New System.Drawing.Size(94, 38)
+        Me.btnActualiar.TabIndex = 3
+        Me.btnActualiar.Text = "Actualizar"
+        Me.btnActualiar.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(551, 371)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(90, 29)
+        Me.btnSalir.TabIndex = 4
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'txtCodigo
+        '
+        Me.txtCodigo.Location = New System.Drawing.Point(162, 210)
+        Me.txtCodigo.Name = "txtCodigo"
+        Me.txtCodigo.Size = New System.Drawing.Size(153, 20)
+        Me.txtCodigo.TabIndex = 5
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(162, 255)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(153, 20)
+        Me.txtNombre.TabIndex = 6
+        '
+        'txtPrecio
+        '
+        Me.txtPrecio.Location = New System.Drawing.Point(162, 304)
+        Me.txtPrecio.Name = "txtPrecio"
+        Me.txtPrecio.Size = New System.Drawing.Size(153, 20)
+        Me.txtPrecio.TabIndex = 7
+        '
+        'txtCategoria
+        '
+        Me.txtCategoria.Location = New System.Drawing.Point(162, 351)
+        Me.txtCategoria.Name = "txtCategoria"
+        Me.txtCategoria.Size = New System.Drawing.Size(153, 20)
+        Me.txtCategoria.TabIndex = 8
+        '
+        'txtVendidas
+        '
+        Me.txtVendidas.Location = New System.Drawing.Point(175, 397)
+        Me.txtVendidas.Name = "txtVendidas"
+        Me.txtVendidas.Size = New System.Drawing.Size(153, 20)
+        Me.txtVendidas.TabIndex = 9
+        '
+        'txtExistentes
+        '
+        Me.txtExistentes.Location = New System.Drawing.Point(175, 446)
+        Me.txtExistentes.Name = "txtExistentes"
+        Me.txtExistentes.Size = New System.Drawing.Size(153, 20)
+        Me.txtExistentes.TabIndex = 10
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Label1.Location = New System.Drawing.Point(40, 210)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(66, 20)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Codigo:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Label2.Location = New System.Drawing.Point(13, 253)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(143, 20)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Nombre producto:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Label3.Location = New System.Drawing.Point(67, 302)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(62, 20)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Precio:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Label4.Location = New System.Drawing.Point(56, 349)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(86, 20)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "Categoria:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Label5.Location = New System.Drawing.Point(13, 397)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(155, 20)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "Unidades vendidas:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Label6.Location = New System.Drawing.Point(9, 446)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(160, 20)
+        Me.Label6.TabIndex = 16
+        Me.Label6.Text = "Unidades existenes:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(653, 492)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtExistentes)
+        Me.Controls.Add(Me.txtVendidas)
+        Me.Controls.Add(Me.txtCategoria)
+        Me.Controls.Add(Me.txtPrecio)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.txtCodigo)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnActualiar)
+        Me.Controls.Add(Me.btnAgregar)
+        Me.Controls.Add(Me.labelTitulo)
+        Me.Controls.Add(Me.dataGridViewProductos)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dataGridViewProductos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents dataGridViewProductos As DataGridView
+    Friend WithEvents labelTitulo As Label
+    Friend WithEvents btnAgregar As Button
+    Friend WithEvents btnActualiar As Button
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents txtCodigo As TextBox
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtPrecio As TextBox
+    Friend WithEvents txtCategoria As TextBox
+    Friend WithEvents txtVendidas As TextBox
+    Friend WithEvents txtExistentes As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ inventarioEmpresa/Form1.vb
@@ -0,0 +1,85 @@
+﻿Public Class Form1
+
+
+    Public conex As New OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=inventario.accdb")
+    Public com As New OleDb.OleDbCommand
+
+
+    Function ConectarDB()
+        Try
+            conex.Open()
+            com.Connection = conex
+            com.CommandType = CommandType.Text
+            Console.WriteLine("Conexion con la base de datos correcta.")
+
+        Catch ex As Exception
+
+            If Err.Number = 5 Then
+                MsgBox("Error al conectar la base de datos." & Err.Description)
+            End If
+
+        End Try
+    End Function
+
+
+    Function limpiarTextBox()
+
+    End Function
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ConectarDB()
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub LimpiarMisTextBox()
+        txtCodigo.Clear()
+        txtNombre.Clear()
+        txtPrecio.Clear()
+        txtCategoria.Clear()
+        txtVendidas.Clear()
+        txtExistentes.Clear()
+    End Sub
+
+    Private Sub btnAgregar_Click(sender As Object, e As EventArgs) Handles btnAgregar.Click
+        Dim codigo, nombre, Precio, categoria, vendidas, existentes As String
+
+
+        codigo = txtCodigo.Text
+        nombre = txtNombre.Text
+        Precio = txtPrecio.Text
+        categoria = txtCategoria.Text
+        vendidas = txtVendidas.Text
+        existentes = txtExistentes.Text
+
+
+
+        Dim sql As String = String.Format("INSERT INTO Inventario(Codigo, nombre_producto,precio,categoria,unid_vendidas,unidad_existentes) VALUES({0},'{1}',{2},'{3}',{4},{5})", codigo, nombre, Precio, categoria, vendidas, existentes)
+
+        Console.WriteLine(sql)
+
+        com.CommandText = sql
+
+        Try
+            com.ExecuteNonQuery()
+            MsgBox("Registro " & codigo & " guardado Nombre: " & nombre & "")
+            LimpiarMisTextBox()
+
+        Catch ex As Exception
+            MsgBox("¡UPS!, un error acaba de ocurrir " & Err.Description)
+        End Try
+
+    End Sub
+
+    Private Sub btnActualiar_Click(sender As Object, e As EventArgs) Handles btnActualiar.Click
+        Dim sqlActualizados As String = String.Format("SELECT * FROM Inventario")
+        Dim adapt As New OleDb.OleDbDataAdapter(sqlActualizados, conex)
+        Dim obtenerDatos As New DataSet
+        adapt.Fill(obtenerDatos, "codigo")
+        dataGridViewProductos.DataSource = obtenerDatos
+        dataGridViewProductos.DataMember = "codigo"
+
+
+    End Sub
+End Class
